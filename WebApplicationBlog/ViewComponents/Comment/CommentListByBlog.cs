2430a80aae101f1573e436e9c37a3e87b08d0e32
yumalieser <yumalieser@gmail.com>
2023-01-17 19:34:22 +0300

    v.1.3

---
+++ WebApplicationBlog/ViewComponents/Comment/CommentListByBlog.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace WebApplicationBlog.ViewComponents.Comment
+{
+    public class CommentListByBlog : ViewComponent
+    {
+        CommentManager cm = new CommentManager(new EfCommentRepository());
+        public IViewComponentResult Invoke()
+        {
+            var values = cm.GetList(1);
+            return View(values);
+        }
+    }
+}
---
+++ WebApplicationBlog/ViewComponents/CommentList.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApplicationBlog.Models;
+
+namespace WebApplicationBlog.ViewComponents
+{
+    public class CommentList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentvalues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    Id = 1,
+                    Username = "Ali"
+                },
+                new UserComment {
+                    Id = 2,
+                    Username = "Mahmut"
+                },
+                new UserComment{
+                    Id = 3,
+                    Username = "Ceren"
+                }
+            };
+            return View(commentvalues);
+        }
+    }
+}
