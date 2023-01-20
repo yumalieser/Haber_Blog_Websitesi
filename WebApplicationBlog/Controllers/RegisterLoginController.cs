b5052ef4686c4cfb0d4d081d531e08596380b3b6
yumalieser <yumalieser@gmail.com>
2023-01-21 02:19:38 +0300

    v1.4

---
+++ WebApplicationBlog/Controllers/RegisterLoginController.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace WebApplicationBlog.Controllers
+{
+    public class RegisterLoginController : Controller
+    {
+        WriterManager wm = new WriterManager(new EfWriterRepository());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+            
+        }
+
+        [HttpPost]
+        public IActionResult Index(Writer p)
+        {
+            p.WriterStatus = true;
+            p.WriterAbout = "Merhaba ben yeni bir yazarım.";
+            wm.WriterAdd(p);
+            return RedirectToAction("Index","Blog");
+
+        }
+    }
+}
