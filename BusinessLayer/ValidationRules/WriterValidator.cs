3489ab46ba3772c14a7c79c8ecc2f8fb2a017762
yumalieser <yumalieser@gmail.com>
2023-01-22 19:10:47 +0300

    v1.4.1

---
+++ BusinessLayer/ValidationRules/WriterValidator.cs
@@ -0,0 +1,22 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class WriterValidator : AbstractValidator<Writer>
+    {
+        public WriterValidator()
+        {
+            RuleFor(x=>x.WriterName).NotEmpty().WithMessage("Yazar Adı Soyadı kısmı boş geçilemez !");
+            RuleFor(x=>x.WriterName).MinimumLength(2).WithMessage("Lütfen en az karakter sınırına uyan bir isim girin !");
+            RuleFor(x=>x.WriterName).MaximumLength(60).WithMessage("Lütfen maximum karakter sınırına uyan bir isim girin !");
+            RuleFor(x=>x.WriterMail).NotEmpty().WithMessage("Mail adresi boş geçilemez !");
+            RuleFor(x=>x.WriterPassword).NotEmpty().WithMessage("Şifre kısmı boş geçilemez !");
+        }
+    }
+}
---
+++ WebApplicationBlog/Controllers/RegisterController.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace WebApplicationBlog.Controllers
+{
+    public class RegisterController : Controller
+    {
+        WriterManager wm = new WriterManager(new EfWriterRepository());
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+            
+        }
+
+        [HttpPost]
+        public IActionResult Index(Writer p)
+        {
+            WriterValidator wv = new WriterValidator();
+            ValidationResult results = wv.Validate(p);
+            if (results.IsValid)
+            {
+                p.WriterStatus = true;
+                p.WriterAbout = "Merhaba ben yeni bir yazarım.";
+                wm.WriterAdd(p);
+                return RedirectToAction("Index", "Blog");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
