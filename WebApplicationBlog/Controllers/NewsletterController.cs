6aef0ff4a7c2da61037c29a6ca3a1451baaacd85
yumalieser <yumalieser@gmail.com>
2023-01-23 00:03:23 +0300

    v1.5

---
+++ WebApplicationBlog/Controllers/NewsletterController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationBlog.Controllers
+{
+    public class NewsletterController : Controller
+    {
+        NewsLetterManager nm = new NewsLetterManager(new EfNewsLetterRepository());
+
+        [HttpGet]
+        public PartialViewResult SubscribeMail()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public IActionResult SubscribeMail(NewsLetter p)
+        {
+            p.MailStatus=true;
+            nm.AddNewsLetter(p);
+            return RedirectToAction("Index", "Blog");
+        }
+    }
+}
