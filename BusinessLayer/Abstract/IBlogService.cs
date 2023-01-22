21028373af267905a43b9fe9cf4f531d728a183f
yumalieser <yumalieser@gmail.com>
2023-01-22 23:05:06 +0300

    v1.4.2

--- BusinessLayer/Abstract/IBlogService.cs
+++ BusinessLayer/Abstract/IBlogService.cs
@@ -16,6 +16,7 @@ namespace BusinessLayer.Abstract
         Blog GetById(int id);
 
         List<Blog> GetBlogListWithCategory();
+        List<Blog> GetBlogListByWriter(int id);
 
     }
 }
---
+++ WebApplicationBlog/ViewComponents/Blog/WriterLastBlog.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationBlog.ViewComponents.Blog
+{
+    public class WriterLastBlog : ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = bm.GetBlogListByWriter(1);
+            return View(values);
+        }
+    }
+}
